27477ed5fcd63f7600794b89606fdb7e1a1913a7
diksha-giri013 <diksha.giri013@gmail.com>
2019-07-11 13:36:37 +0530

    Finalized CS

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -4,9 +4,8 @@ import { Link } from '@reach/router';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './Creator.css';
-//import ReaderNavigator from './ReaderNavigator';
-//import Reader from './Reader';
 
+//Function to generate unique ids for sections
 function makeid(length) {
 	var result = '';
 	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -16,44 +15,44 @@ function makeid(length) {
 	}
 	return result;
 }
+const [content, media, subHeading] = ['content', 'media', 'subHeading'];
+const dummyMedia =
+	'https://images.unsplash.com/photo-1562356759-d42686d03a61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80';
 
-let myModule = {
-	heading: ['Main', 'Heading'],
-	metaData: ['01/09/2109', 'Anonymous'],
-	sectionData: {
-		subHeading: [''],
-		content: [''],
-		media: [
-			'https://images.unsplash.com/photo-1562356759-d42686d03a61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
-		],
-	},
-};
-
+/***********************************************************************************/
+/*------------------------------component for Sections-----------------------------*/
+/***********************************************************************************/
 const Section = (props) => {
-	const [editorState, setEditorState] = useState(props.sectionData || '');
-	const [form, setFormState] = useState({
-		subheading: props.subheading,
-		media:
-			props.media ||
-			'https://images.unsplash.com/photo-1562356759-d42686d03a61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
-	});
+	const sno = props.sno;
+	const sectionContent = props.sectionContent;
+	const subheading = props.subheading;
+	const defaultMedia = props.media;
 
+	//Hook for Quill State
+	const [editorState, setEditorState] = useState(sectionContent || '');
+
+	//Hook for word count of section content
+	const [wordCounter, setWordCounter] = useState(editorState.split(' ').length);
+
+	//Hook for Heading and Media form fields
+	const [form, setFormState] = useState({
+		subheading: subheading,
+		media: defaultMedia,
+	});
+	/*************************Event handlers for hooks of form and Quill State***************************/
 	const handleOnChange = (value) => {
 		setEditorState(value);
-		setWordCounter(editorState.split(' ').length);
-		myModule.sectionData.content[props.sno - 1] = editorState;
+		setWordCounter(value.split(' ').length);
+		props.modifyModuleData(sno - 1, content, value);
 	};
-
 	const handleSubheadingChange = (e) => {
 		setFormState({ ...form, subheading: e.target.value });
-		myModule.sectionData.subHeading[props.sno - 1] = form.subheading;
+		props.modifyModuleData(sno - 1, subHeading, e.target.value);
 	};
 	const handleMediaChange = (e) => {
 		setFormState({ ...form, media: e.target.value });
-		myModule.sectionData.media[props.sno - 1] = form.media;
+		props.modifyModuleData(sno - 1, media, e.target.value);
 	};
-
-	const [wordCounter, setWordCounter] = useState(editorState.split(' ').length);
 
 	return (
 		<React.Fragment>
@@ -61,7 +60,7 @@ const Section = (props) => {
 				<div>
 					<form className='section-form'>
 						<input
-							className='subheading'
+							className='form-subheading'
 							onChange={handleSubheadingChange}
 							value={form.subheading}
 							id='subHeading'
@@ -79,6 +78,7 @@ const Section = (props) => {
 					<form className='section-form'>
 						<h4>Media : </h4>{' '}
 						<input
+							className='form-media'
 							onChange={handleMediaChange}
 							value={form.media}
 							id='media'
@@ -89,7 +89,12 @@ const Section = (props) => {
 				</div>
 			</div>
 			<div className='divider'>
-				<button onClick={() => props.handleDelete(props.sno - 1)}>-Delete-</button>
+				<button className='green' onClick={() => props.handleAdd(props.sno)}>
+					-Add-
+				</button>
+				<button className='red' onClick={() => props.handleDelete(props.sno - 1)}>
+					-Delete-
+				</button>
 				<br />
 				Section No. - {props.sno} | Word Count - {wordCounter}
 			</div>
@@ -97,6 +102,9 @@ const Section = (props) => {
 	);
 };
 
+/***********************************************************************************/
+/*---------------------------component for Main Creator----------------------------*/
+/***********************************************************************************/
 const Creator = (props) => {
 	const mod = {
 		heading: [''],
@@ -104,87 +112,30 @@ const Creator = (props) => {
 		sectionData: {
 			subHeading: [''],
 			content: [''],
-			media: [
-				'https://images.unsplash.com/photo-1562356759-d42686d03a61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
-			],
+			media: [dummyMedia],
 		},
 	};
 	const module = useSelector((state) => state.courses[props.course][props.module] || mod);
-	myModule.heading = [...module.heading];
-	myModule.metaData = [...module.metaData];
-	myModule.sectionData.subHeading = [...module.sectionData.subHeading];
-	myModule.sectionData.content = [...module.sectionData.content];
-	myModule.sectionData.media = [...module.sectionData.media];
 
-	let initialId = [];
+	//Hook for collection of sections
 	const [myArray, setMyArray] = useState(() => {
+		let initialId = [];
 		module.sectionData.content.forEach(() => {
 			initialId.push(makeid(5));
 		});
 		return initialId;
 	});
 
-	const [moduleData, setModuleData] = useState(myModule);
-	const dispatch = useDispatch();
+	//Hook for Module Data
+	const [moduleData, setModuleData] = useState(module);
 
-	const handleAdd = () => {
-		setMyArray([...myArray, makeid(5)]);
-	};
-
-	const handleDelete = (id) => {
-		const arr = [...myArray];
-		setMyArray(arr.filter((element) => arr.indexOf(element) !== id));
-		const newContent = myModule.sectionData.content.filter(
-			(element) => myModule.sectionData.content.indexOf(element) !== id
-		);
-		const newMedia = myModule.sectionData.media.filter(
-			(element) => myModule.sectionData.media.indexOf(element) !== id
-		);
-		const newSubHeading = myModule.sectionData.subHeading.filter(
-			(element) => myModule.sectionData.subHeading.indexOf(element) !== id
-		);
-
-		dispatch({
-			type: 'DATA_UPDATE_COURSES',
-			heading: [
-				headingForm.heading.slice(0, headingForm.heading.lastIndexOf(' ')),
-				headingForm.heading.split(' ').splice(-1),
-			],
-			author: headingForm.author,
-			date: headingForm.date,
-			subHeading: newSubHeading,
-			media: newMedia,
-			content: newContent,
-			course: props.course,
-			module: props.module,
-		});
-	};
-
-	const handleSubmit = () => {
-		setModuleData(myModule);
-		console.log(myModule);
-		dispatch({
-			type: 'DATA_UPDATE_COURSES',
-			heading: [
-				headingForm.heading.slice(0, headingForm.heading.lastIndexOf(' ')),
-				headingForm.heading.split(' ').splice(-1),
-			],
-			author: headingForm.author,
-			date: headingForm.date,
-			subHeading: moduleData.sectionData.subHeading,
-			media: moduleData.sectionData.media,
-			content: moduleData.sectionData.content,
-			course: props.course,
-			module: props.module,
-		});
-	};
-
+	//Hook for heading form
 	const [headingForm, setHeadingForm] = useState({
-		heading: module.heading.join(' '),
-		author: module.metaData[1],
-		date: module.metaData[0],
+		heading: moduleData.heading.join(' '),
+		author: moduleData.metaData[1],
+		date: moduleData.metaData[0],
 	});
-
+	/*************************Event handlers for hooks of heading form***************************/
 	const handleHeadingChange = (e) => {
 		setHeadingForm({ ...headingForm, heading: e.target.value });
 	};
@@ -195,6 +146,89 @@ const Creator = (props) => {
 
 	const handleDateChange = (e) => {
 		setHeadingForm({ ...headingForm, date: e.target.value });
+	};
+
+	/********************Event handlers for Section Modification (ADD DELETE CHANGE)**************************/
+	const handleAdd = (id) => {
+		let newArray = [...myArray];
+		newArray.splice(id, 0, makeid(5));
+		setMyArray(newArray);
+
+		let newContent = [...moduleData.sectionData.content];
+		let newMedia = [...moduleData.sectionData.media];
+		let newSubHeading = [...moduleData.sectionData.subHeading];
+
+		newContent.splice(id, 0, '');
+		newSubHeading.splice(id, 0, '');
+		newMedia.splice(id, 0, dummyMedia);
+
+		setModuleData({
+			...moduleData,
+			sectionData: {
+				subHeading: [...newSubHeading],
+				content: [...newContent],
+				media: [...newMedia],
+			},
+		});
+	};
+
+	const modifyModuleData = (id, param, value) => {
+		let newContent = [...moduleData.sectionData.content];
+		let newMedia = [...moduleData.sectionData.media];
+		let newSubHeading = [...moduleData.sectionData.subHeading];
+		if (param === 'content') newContent[id] = value;
+		if (param === 'media') newMedia[id] = value;
+		if (param === 'subHeading') newSubHeading[id] = value;
+
+		setModuleData({
+			...moduleData,
+			sectionData: {
+				subHeading: [...newSubHeading],
+				content: [...newContent],
+				media: [...newMedia],
+			},
+		});
+	};
+
+	const handleDelete = (id) => {
+		const arr = [...myArray];
+		setMyArray(arr.filter((element) => arr.indexOf(element) !== id));
+		const newContent = moduleData.sectionData.content.filter(
+			(element) => moduleData.sectionData.content.indexOf(element) !== id
+		);
+		const newMedia = moduleData.sectionData.media.filter(
+			(element) => moduleData.sectionData.media.indexOf(element) !== id
+		);
+		const newSubHeading = moduleData.sectionData.subHeading.filter(
+			(element) => moduleData.sectionData.subHeading.indexOf(element) !== id
+		);
+		setModuleData({
+			...moduleData,
+			sectionData: {
+				subHeading: [...newSubHeading],
+				content: [...newContent],
+				media: [...newMedia],
+			},
+		});
+	};
+
+	//EVENT HANDLE FOR COMMITING TO STATE USING REDUX
+	const dispatch = useDispatch();
+	const handleSubmit = () => {
+		dispatch({
+			type: 'DATA_UPDATE_COURSES',
+			heading: [
+				headingForm.heading.slice(0, headingForm.heading.lastIndexOf(' ')),
+				...headingForm.heading.split(' ').splice(-1),
+			],
+			author: headingForm.author,
+			date: headingForm.date,
+			subHeading: moduleData.sectionData.subHeading,
+			media: moduleData.sectionData.media,
+			content: moduleData.sectionData.content,
+			course: props.course,
+			module: props.module,
+		});
 	};
 
 	return (
@@ -228,14 +262,15 @@ const Creator = (props) => {
 					<Section
 						key={unique}
 						sno={id + 1}
-						sectionData={module.sectionData.content[id]}
-						subheading={module.sectionData.subHeading[id]}
-						media={module.sectionData.media[id]}
+						sectionContent={moduleData.sectionData.content[id]}
+						subheading={moduleData.sectionData.subHeading[id]}
+						media={moduleData.sectionData.media[id]}
 						handleDelete={handleDelete}
+						handleAdd={handleAdd}
+						modifyModuleData={modifyModuleData}
 					/>
 				))}
 				<div className='control'>
-					<button onClick={handleAdd}>-Add-</button> <br />
 					<button onClick={handleSubmit}>-Save-</button> <br />
 					<Link className='linker' to={`/reader/${props.course}/${props.module}`}>
 						Preview
